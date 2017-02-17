14bc8df9a9fad474e80224f2f8d3e5e04094b7df
Nicoleta Chirchis <nicoleta.chirchis@yandex.ru>
2017-02-17 04:40:39 +0200

    Revert "Завершение верстку личного проекта"
    This reverts commit d8d2ad74d60e8406a8f98180204eaddd44c226cc.

--- js/map.js
+++
@@ -1,6 +0,0 @@
-var link = document.querySelector(".search_btn");
-link.addEventListener("click", function(event){
-	event.preventDefault(); 
-	var search = document.querySelector(".search_form");
-	search.classList.add("search_form_show");
-});
