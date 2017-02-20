8a08a803c47beadb453b1d87b4e59ce7a3bc2a6e
Nicoleta Chirchis <nicoleta.chirchis@yandex.ru>
2017-02-20 23:03:42 +0200

    Исправление крт Б11.
    --- в форме блоки для указания количества взрослых и детей не
    соответствуют размерам и не верное выравнивание иконок (+ и -)
    --- в кнопках на странице каталога отелей текст не корректно выровнен

--- js/map.js
+++ js/map.js
@@ -2,5 +2,5 @@ var link = document.querySelector(".search_btn");
 var search = document.querySelector(".search_form");
 link.addEventListener("click", function(event){
 	event.preventDefault(); 
-	search.classList.toggle("search_form_show");
+	search.classList.toggle("search_form_hidden");
 });
